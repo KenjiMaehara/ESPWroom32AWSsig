b94ff1c0b9ac3f21e403fb2feb078a63c1097634
KenjiMaehara <kmaehara@mel.co.jp>
2019-02-09 15:24:17 +0900

    インデント調整
    インデント調整

--- wifiAutoConnection/AWSConnect.ino
+++ wifiAutoConnection/AWSConnect.ino
@@ -5,7 +5,7 @@ String RootCAComplete;
 bool awsRootCA(fs::FS &fs, String path) {
   //File file = SD_MMC.open(path, "r");
   File file = fs.open(path, "r");
-  
+
   if (!file) {
     Serial.printf("Can't Open File %s", path);
     return false;
@@ -16,9 +16,9 @@ bool awsRootCA(fs::FS &fs, String path) {
     keygen = file.readStringUntil('\n');
     RootCAComplete += keygen + "\n";
   }
-  
+
   Root_CA = RootCAComplete.c_str();
-  
+
   //Serial.printf("data check Root_CA: %s\n", Root_CA);
   //Serial.printf("data check: %s\n", keygenComplete.c_str());
   return true;
@@ -34,7 +34,7 @@ String privateComplite;
 bool awsPrivateKey(fs::FS &fs, String path) {
   //File file = SD_MMC.open(path, "r");
   File file = fs.open(path, "r");
-  
+
   if (!file) {
     Serial.printf("Can't Open File %s", path);
     return false;
@@ -45,9 +45,9 @@ bool awsPrivateKey(fs::FS &fs, String path) {
     keygen = file.readStringUntil('\n');
     privateComplite += keygen + "\n";
   }
-  
+
   Client_private = privateComplite.c_str();
-  
+
   //Serial.printf("data check Client_private: %s\n", Client_private);
   //Serial.printf("data check: %s\n", keygenComplete.c_str());
   return true;
@@ -60,7 +60,7 @@ String ClientCertComplete;
 bool awsClientCert(fs::FS &fs, String path) {
   //File file = SD_MMC.open(path, "r");
   File file = fs.open(path, "r");
-  
+
   if (!file) {
     Serial.printf("Can't Open File %s", path);
     return false;
@@ -71,9 +71,9 @@ bool awsClientCert(fs::FS &fs, String path) {
     keygen = file.readStringUntil('\n');
     ClientCertComplete += keygen + "\n";
   }
-  
+
   Client_cert = ClientCertComplete.c_str();
-  
+
   //Serial.printf("data check Client_cert: %s\n", Client_cert);
   //Serial.printf("data check: %s\n", keygenComplete.c_str());
   return true;
@@ -108,7 +108,7 @@ String secCampanyData;
 bool getSDCardData(fs::FS &fs, String path,int dataTypeNum) {
   //File file = SD_MMC.open(path, "r");
   File file = fs.open(path, "r");
-  
+
   if (!file) {
     Serial.printf("Can't Open File %s \n", path);
     return false;
@@ -117,17 +117,17 @@ bool getSDCardData(fs::FS &fs, String path,int dataTypeNum) {
     String line = file.readStringUntil('\n');
     line = line.substring(0, line.indexOf('#'));
     //Serial.printf("DeviceID: %s \n", line.c_str());
-    
+
     //String deviceCheck = line.substring(0, line.indexOf("device name"));
 
     if(dataTypeNum == NUMDeviceID){
-      
+
       if (line.indexOf("device name") != -1) {
         deviceIDString = line.substring(line.indexOf(':')+1); deviceIDString.trim();
         deviceID = (char *)deviceIDString.c_str();
         return true;
       }
-    
+
     }else if(dataTypeNum == NUMServer){
 
       if (line.indexOf("aws server") != -1) {
@@ -135,7 +135,7 @@ bool getSDCardData(fs::FS &fs, String path,int dataTypeNum) {
         server = serverAddres.c_str();
         return true;
       }
-      
+
     }else if(dataTypeNum == NUMPort){
 
       if (line.indexOf("aws server port") != -1) {
@@ -151,7 +151,7 @@ bool getSDCardData(fs::FS &fs, String path,int dataTypeNum) {
         pubTopic = (char *)pubTopicData.c_str();
         return true;
       }
-      
+
     }else if(dataTypeNum == NUMSubTopic){
 
       if (line.indexOf("Sub Topic") != -1) {
@@ -159,7 +159,7 @@ bool getSDCardData(fs::FS &fs, String path,int dataTypeNum) {
         subTopic = (char *)subTopicData.c_str();
         return true;
       }
-      
+
     }else if(dataTypeNum == NUMSecCampany){
 
       if (line.indexOf("Secrity Company") != -1) {
@@ -169,47 +169,47 @@ bool getSDCardData(fs::FS &fs, String path,int dataTypeNum) {
       }
 
     }
-    
-    
+
+
   }
   Serial.printf("DeviceID is no data \n");
 
-    if(dataTypeNum == NUMDeviceID){
-      
-        deviceIDString = "noData";
-        deviceID = (char *)deviceIDString.c_str();
-    
-    }else if(dataTypeNum == NUMServer){
+  if(dataTypeNum == NUMDeviceID){
 
-        serverAddres = "noData";
-        server = serverAddres.c_str();
+    deviceIDString = "noData";
+    deviceID = (char *)deviceIDString.c_str();
 
-    }else if(dataTypeNum == NUMPort){
+  }else if(dataTypeNum == NUMServer){
 
-        portNumber = "8883";
-        port = portNumber.toInt();
+    serverAddres = "noData";
+    server = serverAddres.c_str();
 
-    }else if(dataTypeNum == NUMPubTopic){
+  }else if(dataTypeNum == NUMPort){
 
-        pubTopicData = "noData";
-        pubTopic = (char *)pubTopicData.c_str();
-      
-    }else if(dataTypeNum == NUMSubTopic){
+    portNumber = "8883";
+    port = portNumber.toInt();
 
-        subTopicData = "noData";
-        subTopic = (char *)subTopicData.c_str();
+  }else if(dataTypeNum == NUMPubTopic){
 
-    }else if(dataTypeNum == NUMSecCampany){
+    pubTopicData = "noData";
+    pubTopic = (char *)pubTopicData.c_str();
 
-        secCampanyData = "noData";
-        secCampany = (char *)secCampanyData.c_str();
-    }
-  
+  }else if(dataTypeNum == NUMSubTopic){
+
+    subTopicData = "noData";
+    subTopic = (char *)subTopicData.c_str();
+
+  }else if(dataTypeNum == NUMSecCampany){
+
+    secCampanyData = "noData";
+    secCampany = (char *)secCampanyData.c_str();
+  }
+
   return false;
 }
 
 
- 
+
 void setup(){
   Serial.begin(115200);
   if(!SD.begin()){
@@ -218,12 +218,12 @@ void setup(){
   }
 
   uint8_t cardType = SD.cardType();
- 
+
   if(cardType == CARD_NONE){
     Serial.println("No SD card attached");
     return;
   }
- 
+
   Serial.print("SD Card Type: ");
   if(cardType == CARD_MMC){
     Serial.println("MMC");
@@ -234,7 +234,7 @@ void setup(){
   } else {
     Serial.println("UNKNOWN");
   }
- 
+
   uint64_t cardSize = SD.cardSize() / (1024 * 1024);
   Serial.printf("SD Card Size: %lluMB\n", cardSize);
 
@@ -258,15 +258,15 @@ void setup(){
 
 
   awsRootCA(SD, "/RootCA.pem");
-  
+
   awsClientCert(SD, "/certificate.pem.crt") ;
   awsPrivateKey(SD, "/private.pem.key");
-  
+
 
   getSDCardData(SD, "/config", NUMServer);
   getSDCardData(SD, "/config", NUMPort);
 
-  
+
   getSDCardData(SD, "/config", NUMPubTopic);
   getSDCardData(SD, "/config", NUMSubTopic);
 
@@ -289,17 +289,17 @@ void setup(){
   //String tempWord = "/";
   //String tempWord02 = "/";
   if (secCampanyData.indexOf("noData") == -1){
-     //pubTopicData += tempWord;
-     pubTopicData += "/" + secCampanyData;
-     pubTopic = (char *)pubTopicData.c_str();
-     
-     //subTopicData += tempWord02;
-     subTopicData += "/" + secCampanyData;
-     subTopic = (char *)subTopicData.c_str();
+    //pubTopicData += tempWord;
+    pubTopicData += "/" + secCampanyData;
+    pubTopic = (char *)pubTopicData.c_str();
+
+    //subTopicData += tempWord02;
+    subTopicData += "/" + secCampanyData;
+    subTopic = (char *)subTopicData.c_str();
   }
 
 
-  
+
   Serial.printf("aws Pub Topic 02: %s \n", pubTopic);
   Serial.printf("aws Sub Topic 02: %s \n", subTopic);
   Serial.printf("security campany: %s \n", secCampany);
@@ -322,19 +322,19 @@ int value = 0;
 
 
 void connectAWSIoT() {
-    while (!mqttClient.connected()) {
-        if (mqttClient.connect(deviceID)) {
-            Serial.println("Connected.");
-            int qos = 0;
-            mqttClient.subscribe(subTopic, qos);
-            Serial.println("Subscribed.");
-        } else {
-            Serial.print("Failed. Error state=");
-            Serial.print(mqttClient.state());
-            // Wait 5 seconds before retrying
-            delay(5000);
-        }
+  while (!mqttClient.connected()) {
+    if (mqttClient.connect(deviceID)) {
+      Serial.println("Connected.");
+      int qos = 0;
+      mqttClient.subscribe(subTopic, qos);
+      Serial.println("Subscribed.");
+    } else {
+      Serial.print("Failed. Error state=");
+      Serial.print(mqttClient.state());
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
+  }
 }
 
 void setupAWS()
@@ -346,14 +346,14 @@ void setupAWS()
   client.setCACert(Root_CA);
   client.setCertificate(Client_cert);
   client.setPrivateKey(Client_private);
-  mqttClient.setServer(server, port);  
+  mqttClient.setServer(server, port);
   mqttClient.setCallback(mqttCallback);
 
   connectAWSIoT();
 
   pinMode(26, OUTPUT);
   pinMode(27, INPUT_PULLUP);
-  
+
 }
 
 
@@ -362,44 +362,44 @@ int Value = 0;
 char pubMessage[128];
 
 void mqttCallback (char* topic, byte* payload, unsigned int length) {
-    Serial.print("Received. topic=");
-    Serial.println(topic);
-    for (int i = 0; i < length; i++) {
-        Serial.print((char)payload[i]);
-    }
-    Serial.print("\n");
+  Serial.print("Received. topic=");
+  Serial.println(topic);
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.print("\n");
 
 
-    //char s1[] = "abcdefgij";
-    char s2[] = " sec ";
-    char s3[] = " set ";
-    char s4[] = " reset ";
-    char *ret;
-    char *ret2;
+  //char s1[] = "abcdefgij";
+  char s2[] = " sec ";
+  char s3[] = " set ";
+  char s4[] = " reset ";
+  char *ret;
+  char *ret2;
 
-    if ((ret = strstr((char *)payload, s2)) != NULL && (ret2 = strstr((char *)payload, s3)) != NULL ) {
-        snprintf (msg, 75, "%sは%d番目にありました．\n", s2, ret - (char *)payload);
-        Serial.println(msg);
-        snprintf (msg, 75, "%sは%d番目にありました．\n", s3, ret2 - (char *)payload);
-        Serial.println(msg);
-        Serial.print("sec set \n");
-        digitalWrite(26, HIGH);
-        Serial.println(msg);
-        
-    } else if ((ret = strstr((char *)payload, s2)) != NULL && (ret2 = strstr((char *)payload, s4)) != NULL ) {
-        snprintf (msg, 75, "%sは%d番目にありました．\n", s2, ret - (char *)payload);
-        Serial.println(msg);
-        snprintf (msg, 75, "%sは%d番目にありました．\n", s3, ret2 - (char *)payload);
-        Serial.println(msg);
-        Serial.print("sec reset \n");
-        digitalWrite(26, LOW);
-        Serial.println(msg);    
-    
-    
-    } else {
-        snprintf (msg, 75, "%sはありませんでした．\n", s2);
-        Serial.println(msg);
-    }  
+  if ((ret = strstr((char *)payload, s2)) != NULL && (ret2 = strstr((char *)payload, s3)) != NULL ) {
+    snprintf (msg, 75, "%sは%d番目にありました．\n", s2, ret - (char *)payload);
+    Serial.println(msg);
+    snprintf (msg, 75, "%sは%d番目にありました．\n", s3, ret2 - (char *)payload);
+    Serial.println(msg);
+    Serial.print("sec set \n");
+    digitalWrite(26, HIGH);
+    Serial.println(msg);
+
+  } else if ((ret = strstr((char *)payload, s2)) != NULL && (ret2 = strstr((char *)payload, s4)) != NULL ) {
+    snprintf (msg, 75, "%sは%d番目にありました．\n", s2, ret - (char *)payload);
+    Serial.println(msg);
+    snprintf (msg, 75, "%sは%d番目にありました．\n", s3, ret2 - (char *)payload);
+    Serial.println(msg);
+    Serial.print("sec reset \n");
+    digitalWrite(26, LOW);
+    Serial.println(msg);
+
+
+  } else {
+    snprintf (msg, 75, "%sはありませんでした．\n", s2);
+    Serial.println(msg);
+  }
 }
 
 
@@ -414,32 +414,32 @@ void mqttLoop02() {
 
   if(digitalRead(27)==LOW)
   {
-    
+
     if(buttonState==0)
     {
-        buttonState = 1;     
+      buttonState = 1;
 
-        value02++;
-        if(value02 > 1)
-            value02=0;
+      value02++;
+      if(value02 > 1)
+      value02=0;
 
 
       if (!mqttClient.connected()) {
-          connectAWSIoT();
+        connectAWSIoT();
       }
-      
+
       //++value02;
       if(value02==0)
       {
-          snprintf (msg, 75, " sec reset #%ld , %s ", value02, deviceID);
-          digitalWrite(26, LOW);
+        snprintf (msg, 75, " sec reset #%ld , %s ", value02, deviceID);
+        digitalWrite(26, LOW);
       }
       else
       {
-          snprintf (msg, 75, " sec set #%ld , %s ", value02, deviceID);
-          digitalWrite(26, HIGH);
+        snprintf (msg, 75, " sec set #%ld , %s ", value02, deviceID);
+        digitalWrite(26, HIGH);
       }
-      
+
       Serial.print("Publish message: ");
       Serial.println(msg);
       mqttClient.publish(pubTopic, msg);
@@ -449,11 +449,11 @@ void mqttLoop02() {
   }
   else
   {
-      if(buttonState==1)
-      {
-         buttonState=0;
-         delay(250);
-      }
-    
+    if(buttonState==1)
+    {
+      buttonState=0;
+      delay(250);
+    }
+
   }
 }
